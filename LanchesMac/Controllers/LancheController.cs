7d34b1ad92433a6f63bf729cccedb4246881dc30
heliomendesjr <90043730+heliomendesjr@users.noreply.github.com>
2022-03-19 15:12:39 -0300

    Criado pasta ViewModels

--- LanchesMac/Controllers/LancheController.cs
+++ LanchesMac/Controllers/LancheController.cs
@@ -1,4 +1,5 @@
 ﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LanchesMac.Controllers
@@ -13,16 +14,13 @@ namespace LanchesMac.Controllers
 
         public IActionResult List()
         {
-            ViewData["Titulo"] = "Todos os Lanches";
-            ViewData["Data"] = DateTime.Now;
+            //var lanches = _lancheRepository.Lanches;           
+            //return View(lanches);
+            var lanchesListViewModel = new LancheListViewModel();
+            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
+            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
 
-            var lanches = _lancheRepository.Lanches;
-            var totalLanches = lanches.Count();
-
-            ViewBag.Total = "Total de lanches : ";
-            ViewBag.TotalLanches = totalLanches;
-
-            return View(lanches);
+            return View(lanchesListViewModel);
         }
     }
 }
