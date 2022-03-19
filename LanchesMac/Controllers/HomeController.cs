568c3ba00f6b30966a457adae9e91c4a35ccea57
heliomendesjr <90043730+heliomendesjr@users.noreply.github.com>
2022-03-19 14:18:41 -0300

    Criado view list

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -10,15 +10,17 @@ namespace LanchesMac.Controllers
         {
             return View();
         }
-        public IActionResult Demo()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None,
+            NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View(new ErrorViewModel
+            {
+                RequestId = Activity.Current?.Id
+                ?? HttpContext.TraceIdentifier
+            });
         }
     }
 }
